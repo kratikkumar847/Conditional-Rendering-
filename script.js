82c3db0f44451019c1fd970162e6ee2f1eb443b9
kratikkumar847 <kratikkumar847@gmail.com>
2022-08-26 11:10:11 +0530

    My 1st Change

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-
 // Root component option
-const options ={
-    data(){
-        return {
-            appName : 'kratik',
-           showName : true
-        }
-    }
-}
+const options = {
+  data() {
+    return {
+      appName: "kratik",
+      showName: true,
+      cart: ["apple", "banana", "mango", "graps", "orange"],
+      person: {
+        Name: "sakshi",
+        age: 20,
+        subject: "Full Stack",
+      },
+      message: "",
+      agree: true,
+      fruits: []
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart.push("x");
+    },
+  },
+};
 
-// Application instance 
+// Application instance
 const app = Vue.createApp(options);
 // View model instance of component
-const vm = app.mount('#app');
+const vm = app.mount("#app");
